c7aeec4a888022e3b7674911b58f66b5e74dd521
hyunjun <homelus@daum.net>
2019-06-23 17:34:31 +0900

    1. 테스트 코드 방법 추가

---
+++ service/src/test/java/java9/flow/FlowTests.java
@@ -0,0 +1,32 @@
+package java9.flow;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.SubmissionPublisher;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+
+public class FlowTests {
+
+    @Test
+    public void FlowTest() {
+
+        // 주어진 조건
+        SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
+        EndSubscriber<String> subscriber = new EndSubscriber<>();
+
+        publisher.subscribe(subscriber);
+
+        List<String> items = List.of("1", "x", "2", "x", "3", "x");
+
+        assertThat(publisher.getNumberOfSubscribers()).isEqualTo(1);
+        items.forEach(publisher::submit);
+        publisher.close();
+
+    }
+
+}
